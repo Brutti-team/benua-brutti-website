b1fe4c7082cc4b9f1d41adcb64d1befab0061429
wanehh <integrator.brutti@gmail.com>
2026-08-21 09:20:47 +0800

    Fix hero to use existing Brutti image asset

--- src/main.jsx
+++ src/main.jsx
@@ -39,12 +39,6 @@ function CollaboratorsPortal() {
 function BruttiSite() {
   useEffect(() => {
     const applyRealAssets = () => {
-      const hero = document.querySelector('.hero__image')
-      if (hero) {
-        hero.src = asset('brutti-team-landscape.jpg')
-        hero.alt = 'Benua Brutti team'
-      }
-
       const storyPhoto = document.querySelector('.story__photo-wrap img')
       if (storyPhoto) {
         storyPhoto.src = asset('founders.jpg')
