d73b9017d4d0b4e40790f1340affc8c637e274be
wanehh <integrator.brutti@gmail.com>
2026-08-26 12:20:35 +0800

    Use landscape Brutti team hero

--- src/main.jsx
+++ src/main.jsx
@@ -40,22 +40,10 @@ function CollaboratorsPortal() {
 function BruttiSite() {
   useEffect(() => {
     const applyRealAssets = () => {
-      const heroWrap = document.querySelector('.hero__image-wrap')
-      const heroPhoto = heroWrap?.querySelector('.hero__image:not(.hero__image--backdrop)')
-
+      const heroPhoto = document.querySelector('.hero__image')
       if (heroPhoto) {
-        heroPhoto.src = asset('brutti team.jpg')
+        heroPhoto.src = asset('brutti-team-landscape.jpg')
         heroPhoto.alt = 'Benua Brutti team'
-        heroPhoto.classList.add('hero__image--full')
-      }
-
-      if (heroWrap && !heroWrap.querySelector('.hero__image--backdrop')) {
-        const backdrop = document.createElement('img')
-        backdrop.src = asset('brutti team.jpg')
-        backdrop.alt = ''
-        backdrop.setAttribute('aria-hidden', 'true')
-        backdrop.className = 'hero__image hero__image--backdrop'
-        heroWrap.insertBefore(backdrop, heroWrap.firstChild)
       }
 
       const storyPhoto = document.querySelector('.story__photo-wrap img')
