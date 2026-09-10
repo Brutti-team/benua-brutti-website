573e577b02a03db96168e44a432f759371abbd56
wanehh <integrator.brutti@gmail.com>
2026-09-10 14:32:26 +0800

    Load desktop refinement for Journey team section

--- src/components/JourneyPage.jsx
+++ src/components/JourneyPage.jsx
@@ -6,6 +6,7 @@ import '../journey-purpose-refine.css'
 import '../journey-team.css'
 import '../journey-team-reference.css'
 import '../journey-team-mobile-final.css'
+import '../journey-team-desktop-refine.css'
 import '../journey-mobile-text-spacing.css'
 import '../journey-hero-mobile-zoomout.css'
 
